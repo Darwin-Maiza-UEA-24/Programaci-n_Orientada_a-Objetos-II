3c99dec62ae780760d4a0cc1be75b86b0a065cb7
Darwin-Maiza-UEA-24 <dd.maizaz@uea.edu.ec>
2025-02-01 20:05:31 -0500

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,50 @@
+import os
+
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+
+
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+
+    opciones = {
+        '1': 'Semana 2/Desarrollo de ejemplos de Tecnicas de programación.py',
+        '2': 'Semana 3/Programación Orientada a Objetos (POO).py',
+        '3': 'Semana 3/Programación tradicional del clima.py',
+        '4': 'Semana 6/Definición de Clase, Definición de Objeto, Herencia, Encapsulación y Polimorfismo en Python.py',
+        '5': 'Semana 7/Constructores (__init__) y destructores (__del__).py'
+
+        # Agrega aquí el resto de las rutas de los scripts
+    }
+
+    while True:
+        print("\n********Menu Principal - Dashboard*************")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+
+
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
